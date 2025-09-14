b2141c5021a2a28897e1f1a900ee9942ad172c2e
seul-pusan <haesul0611@naver.com>
2025-09-14 12:29:58 +0900

    html

--- 1week/02/02.js
+++ 1week/02/02.js
@@ -1,4 +1,18 @@
-const Hello = () => {
+const Hello = (n) => {
     if (n == '1') alert("안녕하세요");
     else alert('Hello');
+
+    console.log('hello test.');
+}
+
+const CheckVar = () => {
+
+    let x;
+    x= 'test';
+
+    const y='20';
+
+    console.log(`x=${typeof(x)}, y=${typeof(y)}`)
+    console.log(`문자열 ${x}은 문자인가요? ${isNaN(x)? '예':'아니오'}`);
+    console.log(`문자열 ${y}은 문자열인가요? ${isNaN(y)? '예':'아니오'}`);
 }
